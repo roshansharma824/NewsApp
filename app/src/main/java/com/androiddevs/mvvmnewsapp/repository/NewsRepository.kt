e56df56dc6afd2dcd142e1ad0a6bdd57499d9a20
roshansharma824 <roshansharma824@gmail.com>
2022-01-15 13:56:29 +0530

    Adding and Deleting Articles

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp.repository
 
 import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.models.Article
 
 class NewsRepository(
     val db: ArticleDatabase
@@ -13,4 +14,9 @@ class NewsRepository(
         RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
 
 
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
